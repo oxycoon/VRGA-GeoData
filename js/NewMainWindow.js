65e2a8fc14aed524ee5fd90e93e6672e8767ed97
oxycoon <daniel.dimby@gmail.com>
2014-10-06 19:09:18 +0200

    trying other methods, inefficient, reverting

--- js/NewMainWindow.js
+++ js/NewMainWindow.js
@@ -21,13 +21,16 @@
 	
 	var clock = new THREE.Clock();
 	
+	var n =0;
+	
 	init();
-	render();
+	//render();
 
 	function init(){
 		scene = new THREE.Scene();
 		
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+		//camera.z = 5;
 		camera.position.x = 1000;
 		camera.position.y = 600;
 		camera.position.z = 1300;
@@ -37,7 +40,8 @@
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		document.body.appendChild(renderer.domElement);
 
-		//testGeo = new THREE.BoxGeometry(1,1,1);
+		//testGeo = new THREE.PlaneGeometry(512, 512, 128, 128);//new THREE.BoxGeometry(1,1,1);
+		//testGeo.applyMatrix(new THREE.Matrix4().makeRotationX( -Math.PI / 2)); //rotates plane
 		//testMatr = new THREE.MeshBasicMaterial({color: 0x00ff00});
 		//testCube = new THREE.Mesh(testGeo, testMatr);
 		//scene.add(testCube);
@@ -45,12 +49,30 @@
 		       // add a light
         var pointLight = new THREE.PointLight(0xFFFFFF);
         scene.add(pointLight);
-        pointLight.position.x = 1000;
+		pointLight.position.x = 1000;
         pointLight.position.y = 3000;
         pointLight.position.z = -1000;
         pointLight.intensity = 8.6;
 		
-		addTerrainUsingHeightMap('../res/maps/narvik.png');
+		addTerrainUsingHeightMap('res/maps/narvik.png');
+/* 		var img = new Image();
+		img.onload = function() {
+			var data = getHeightData(img);
+			
+			var geometry = new THREE.PlaneGeometry(2000, 2000, 256, 256); //bug exists here, can't find reason
+			geometry.applyMatrix(new THREE.Matrix4().makeRotationX( -Math.PI / 2));
+			var texture = THREE.ImageUtils.loadTexture('res/textures/sand.jpg');
+			var material = new THREE.MeshLambertMaterial( { map: texture } );
+			
+			var plane = new THREE.Mesh(geometry, material);
+			
+			for(var i = 0; i < plane.geometry.vertices.length; i++){
+				plane.geometry.vertices[i].y = data[i];
+			}
+			
+			scene.add(plane);
+		};
+		img.src = 'res/maps/narvik.png'; */
 
 
 	}
@@ -64,34 +86,51 @@
 	}
 	
 	//http://www.smartjava.org/content/threejs-render-real-world-terrain-heightmap-using-open-data
+	/**
+	*	
+	*	@param {string} path This string is the path to the height map to load.
+	*/
 	function addTerrainUsingHeightMap(path){
+		//Load heightmap as texture
+		console.log('Loading heightmap: ' + path);
 		var heightMap = THREE.ImageUtils.loadTexture(path);
-		var texture = THREE.ImageUtils.loadTexture('../res/textures/sand.jpg');;
+		//Load diffuse texture
+		console.log('Loading texture: /res/textures/sand.jpg');
+		var texture = THREE.ImageUtils.loadTexture('/res/textures/sand.jpg');
 		
-		var terrainShader = THREE.ShaderTerrain['terrain'];
-		var uniformsTerrain = THREE.UniformsUtils.clone(terrainShader.uniforms);
+		//terrain shader
+		console.log('Initializing terrain shader');
+		var terrainShader = THREE.ShaderTerrain[ 'terrain' ];
+        var uniformsTerrain = THREE.UniformsUtils.clone(terrainShader.uniforms);
 		
+		console.log('Initialing uniforms');
 		//Sets the heightmap
 		uniformsTerrain['tDisplacement'].texture = heightMap;
-		uniformsTerrain['uNormalScale'].value = 1;
+		uniformsTerrain['uDisplacementScale'].value = 100;
 		
+		// how to treat abd scale the normal texture
+        uniformsTerrain[ 'tNormal' ].texture = texture;
+        uniformsTerrain[ 'uNormalScale' ].value = 1;
+
 		uniformsTerrain['tDiffuse1'].texture = texture;
         uniformsTerrain['tDetail'].texture = texture;
         uniformsTerrain['enableDiffuse1'].value = true;
         uniformsTerrain['enableDiffuse2'].value = true;
         uniformsTerrain['enableSpecular'].value = true;
 		
-		uniformsTerrain[ "uDiffuseColor" ].value.setHex(0xcccccc);
-        uniformsTerrain[ "uSpecularColor" ].value.setHex(0xff0000);
+		// diffuse is based on the light reflection
+		uniformsTerrain[ 'uDiffuseColor' ].value.setHex(0xcccccc);
+        uniformsTerrain[ 'uSpecularColor' ].value.setHex(0xff0000);
         // is the base color of the terrain
-        uniformsTerrain[ "uAmbientColor" ].value.setHex(0x0000cc);
+        uniformsTerrain[ 'uAmbientColor' ].value.setHex(0x0000cc);
  
         // how shiny is the terrain
-        uniformsTerrain[ "uShininess" ].value = 3;
+        uniformsTerrain[ 'uShininess' ].value = 3;
  
         // handles light reflection
-        uniformsTerrain[ "uRepeatOverlay" ].value.set(3, 3);
+        uniformsTerrain[ 'uRepeatOverlay' ].value.set(3, 3);
  
+		console.log('Initializing material')
         // configure the material that reflects our terrain
         var material = new THREE.ShaderMaterial({
             uniforms:uniformsTerrain,
@@ -100,23 +139,53 @@
             lights:true,
             fog:false
         });
+		//var material = new THREE.MeshLambertMaterial( { map: texture } );
 		
-		//console.log(heightMap.image.src);
-		
-		//var geometryTerrain = new THREE.PlaneGeometry(heightMap.image.width, heightMap.image.height, heightMap.image.width / 2, heightMap.image.height / 2);
+		console.log('Initializing geometry');
 		var geometryTerrain = new THREE.PlaneGeometry(2000, 2000, 256, 256);
-		geometryTerrain.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI / 2));
+		geometryTerrain.applyMatrix(new THREE.Matrix4().makeRotationX( Math.PI / 2)); //rotates plane
         geometryTerrain.computeFaceNormals();
         geometryTerrain.computeVertexNormals();
         geometryTerrain.computeTangents();
  
         // create a 3D object to add
-        terrain = new THREE.Mesh(geometryTerrain, material);
+		console.log('Initializing mesh');
+        var terrain = new THREE.Mesh(geometryTerrain, material);
         terrain.position.set(0, -125, 0);
         terrain.rotation.x = -Math.PI / 2;
  
         // add the terrain
+		console.log('Adding mesh to scene');
         scene.add(terrain);
+
+	}
+	
+	//http://danni-three.blogspot.no/2013/09/threejs-heightmaps.html
+	function getHeightData(img,scale){
+		if(scale===undefined) scale = 1;
+		
+		var canv = document.createElement('canvas');
+		canv.width = img.width;
+		canv.height = img.height;
+		
+		var cont = canv.getContext('2d');
+		
+		var size = img.width * img.height;
+		data = new Float32Array(size);
+		
+		cont.drawImage(img,0,0);
+		for( var i=0; i < size; i++){
+			data[i] = 0;
+		}
+		var imageData = cont.getImageData(0,0,img.width,img.height);
+		var pixels = imageData.data;
+		
+		var j = 0;
+		for(var i = 0; i < size; i+= 4){
+			var all = pixels[i] + pixels[i+1] + pixels[i+2];
+			data[j++] = all / (12*scale);
+		}
+		return data;
 	}
 
 	
